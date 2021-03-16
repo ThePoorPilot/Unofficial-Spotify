f7e2a3fa45349e7d1e0fefda7cd6580dead4713f
ThePoorPilot <ios8jailbreakpangu@gmail.com>
2021-03-15 20:30:15 -0400

    added variables to build script and did some cleaning

--- Build/install.sh
+++ Build/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 ./build.sh
+#installing
+V_HTTP_VERSION="$(curl -s -H 'Snap-Device-Series: 16' http://api.snapcraft.io/v2/snaps/info/spotify | jq -r '."channel-map"[] | select(.channel.name=='\"stable\"') | .version')" 
 echo "Installing..."
-sudo dpkg -i ./spotify-unofficial_latest_amd64.deb
+sudo dpkg -i ./spotify-unofficial_"$V_HTTP_VERSION"_amd64.deb
