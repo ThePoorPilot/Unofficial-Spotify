db7ad8b9822645a26a0b1dee28d755a803672cb2
ThePoorPilot <ios8jailbreakpangu@gmail.com>
2021-03-20 12:35:30 -0400

    added edge building
    This should additionally check for new edge builds on the github action.

--- auto-build/push.sh
+++ auto-build/push.sh
@@ -10,7 +10,7 @@ then
     git config --global user.name "ThePoorPilot"
     git config --global user.email "ios8jailbreakpangu@gmail.com"
     git add .
-    git commit -m 'changed version number in auto.sh'
+    git commit -m 'changed version number in build checking'
     git push https://ThePoorPilot:"$PASSWORD"@github.com/ThePoorPilot/Unofficial-Spotify.git 
 else
     echo "There isn't a new version, no need to push!"
