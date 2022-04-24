3f86d35daba54bd9b532d1d18a89e1cc7d8cfd32
ThePoorPilot <ios8jailbreakpangu@gmail.com>
2022-04-24 07:12:22 +0000

    changed version number in build checking

--- auto-build/auto-edge.sh
+++ auto-build/auto-edge.sh
@@ -27,13 +27,13 @@ else
 fi
 
 #Check if there is a new package
-if [ "$V_HTTP_VERSION" == "1.1.83.954.gd226dfe8" ]
+if [ "$V_HTTP_VERSION" == "1.1.84.716.gc5f8b819" ]
 then
     echo "no need to re-build. Try again in another hour!"
     exit
 else
     #replace version number in checking
-    sed -i 's/1.1.83.954.gd226dfe8/'"$V_HTTP_VERSION"'/g' ./auto-edge.sh
+    sed -i 's/1.1.84.716.gc5f8b819/'"$V_HTTP_VERSION"'/g' ./auto-edge.sh
     echo 'New Package Found! Version:'"$V_HTTP_VERSION"
 fi
 
